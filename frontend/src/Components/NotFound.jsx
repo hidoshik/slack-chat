db9d57b1d48ca4db1ccbf33b7e87d89b7489957d
Денис Вершков <valeriyabusy@gmail.com>
2025-01-23 09:44:00 +0300

    add NotFound

--- frontend/src/Components/NotFound.jsx
+++ frontend/src/Components/NotFound.jsx
@@ -1,3 +1,11 @@
-export default ({}) => {
-    
+const NotFound = ({}) => {
+    return (
+        <div className="text-center">
+            <img alt="Страница не найдена" className="img-fluid h-25" src="404-D_FLHmTM.svg" height="100px" width="100px"/>
+            <h1 className="h4 text-muted">Страница не найдена</h1>
+            <p className="text-muted">Но вы можете перейти <a href="/">на главную страницу</a></p>
+        </div>
+    )
 }
+
+export default NotFound;
