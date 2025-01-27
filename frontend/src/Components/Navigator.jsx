26ad5deab018cc0320c384861525afd828d064aa
Денис Вершков <valeriyabusy@gmail.com>
2025-01-27 11:23:25 +0300

    add scripts

--- frontend/src/Components/Navigator.jsx
+++ frontend/src/Components/Navigator.jsx
@@ -1,3 +1,3 @@
 export default ({}) => {
-  return ()
+  return (<p>dd</p>)
 };
--- frontend/src/Components/NotFound.jsx
+++ frontend/src/Components/NotFound.jsx
@@ -1,7 +1,7 @@
 const NotFound = ({}) => {
     return (
         <div className="text-center">
-            <img alt="Страница не найдена" className="img-fluid h-25" src="404-D_FLHmTM.svg" height="100px" width="100px"/>
+            <img alt="Страница не найдена" className="img-fluid h-25" src="/404.svg" height="100px" width="100px"/>
             <h1 className="h4 text-muted">Страница не найдена</h1>
             <p className="text-muted">Но вы можете перейти <a href="/">на главную страницу</a></p>
         </div>
