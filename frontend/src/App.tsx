c2a16805da4958d0169a4350f542ce55f49bb399
Денис Вершков <valeriyabusy@gmail.com>
2025-02-15 11:18:01 +0300

    add rollbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,22 +6,43 @@ import Signup from './pages/Signup';
 import NotFound from './pages/NotFound';
 import MainPage from './pages/MainPage';
 import MainLayout from './Components/MainLayout';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import useSocket from './hooks/useSocket';
+
+const rollbarConfig = {
+  accessToken: '5a390a00193b4eae8e44241b462b1136',
+  environment: 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true,
+        guess_uncaught_frames: true
+      }
+    }
+  }
+};
 
 const App = () => {
   useSocket();
 
   return (
-    <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="login" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </MainLayout>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   );
 };
 
